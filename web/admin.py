774c394906b692fab61c43bf7d052931b6bd2a66
Honza Pokorny <me@honza.ca>
2014-08-03 11:19:40 +0200

    Add better item admin

--- web/admin.py
+++ web/admin.py
@@ -2,6 +2,14 @@ from django.contrib import admin
 from models import Item, Buy, UserProfile
 
 
-admin.site.register(Item)
+class ItemAdmin(admin.ModelAdmin):
+    list_filter = ('multi_item', 'already_given', 'surprise', 'user',)
+    list_display = ('name', 'multi_item', 'already_given',)
+
+    class Meta:
+        model = Item
+
+
+admin.site.register(Item, ItemAdmin)
 admin.site.register(Buy)
 admin.site.register(UserProfile)
